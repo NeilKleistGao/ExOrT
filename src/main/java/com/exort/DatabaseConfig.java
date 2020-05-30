2004643e30e7edc0b7d839a776798f4c40ab1240
NeilKleistGao <neilkleistgao@163.com>
2020-05-30 19:36:09 +0800

    add thymeleaf

--- src/main/java/com/exort/DatabaseConfig.java
+++
@@ -1,5 +0,0 @@
-package com.exort;
-
-public class DatabaseConfig {
-
-}
--- src/main/java/com/exort/ExortController.java
+++ src/main/java/com/exort/ExortController.java
@@ -1,18 +1,14 @@
 package com.exort;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
+@Controller
+public class ExortController {
 
-public class ExortController implements Controller {
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        Map<String, String> modelData = new HashMap<String, String>();
-        modelData.put("msg", "rua");
-        return new ModelAndView("index", modelData);
+    @RequestMapping("/")
+    public String getIndex(Model model) {
+        return "index";
     }
 }
