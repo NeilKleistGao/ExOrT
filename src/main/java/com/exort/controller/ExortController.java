c8e46dfd41e681f19c010254cd23863a79993d8e
NeilKleistGao <neilkleistgao@163.com>
2020-06-22 17:33:50 +0800

    add table

--- src/main/java/com/exort/controller/ExortController.java
+++ src/main/java/com/exort/controller/ExortController.java
@@ -8,10 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.logging.SimpleFormatter;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class ExortController {
+
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd");
+    private static SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
 
     @RequestMapping("/")
     public String getIndex(Model model) {
@@ -30,8 +34,33 @@ public class ExortController {
             week = String.valueOf(calendar.get(Calendar.WEEK_OF_YEAR));
         }
 
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, Integer.parseInt(year));
+        calendar.set(Calendar.WEEK_OF_YEAR, Integer.parseInt(week));
+
+        Date start = calendar.getTime();
+
+        for (int i = 0; i < 7; i++) {
+            calendar.setTime(start);
+            calendar.add(Calendar.DATE, i);
+            model.addAttribute("date" + String.valueOf(i), dateFormat.format(calendar.getTime()));
+        }
+
+        calendar.set(Calendar.HOUR_OF_DAY, 7);
+        calendar.set(Calendar.MINUTE, 0);
+
+        List timeline = new LinkedList();
+
+        for (int i = 0; i < 32; i++) {
+            Date d1 = calendar.getTime();
+            calendar.add(Calendar.MINUTE, 30);
+            Date d2 = calendar.getTime();
+            timeline.add(timeFormat.format(d1) + " - " + timeFormat.format(d2));
+        }
+
         model.addAttribute("year", year);
         model.addAttribute("week", week);
+        model.addAttribute("timeline", timeline);
 
         return "timetable";
     }
